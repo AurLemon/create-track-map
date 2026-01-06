c6ad2674b9bee4b69903f507b571493e002eb0df
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-01-06 00:46:31 +0000

    Move back to kotlin DSL
    KFF is somewhat broken under 1.20.1 in regards to access for libs like ktor. How fun.
    
    Dev environment may just have to suffer so that I can get a modernised build out. It does *work* in prod.

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        maven("https://repo.spongepowered.org/maven")
+        maven("https://maven.parchmentmc.org")
+        maven("https://maven.neoforged.net/releases")
+        gradlePluginPortal()
+    }
+}
